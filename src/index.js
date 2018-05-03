c3a008640c7ff1b184b5671eb885fe04ae540ffc
grady-lad <ogradymartin7@gmail.com>
2018-05-03 10:32:00 +0200

    Routing chapter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+
+import "./styles/index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
 const httpLink = new HttpLink({ uri: "http://localhost:4000" });
 
@@ -16,9 +18,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 registerServiceWorker();
